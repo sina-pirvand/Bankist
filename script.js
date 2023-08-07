bc4c2c1cff314006d2597c048678d7489dbed2b8
sina <sinaprvnd.webdev@gmail.com>
2023-08-07 20:34:14 +0330

    login emplement

--- script.js
+++ script.js
@@ -2,28 +2,28 @@
 
 //! Data
 const account1 = {
-  owner: "Jonas Schmedtmann",
+  owner: "Ayda Asadi",
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
 };
 
 const account2 = {
-  owner: "Jessica Davis",
+  owner: "Babak Behtash",
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
 };
 
 const account3 = {
-  owner: "Steven Thomas Williams",
+  owner: "Cin Cin",
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
 };
 
 const account4 = {
-  owner: "Sarah Smith",
+  owner: "Dara Davoudi",
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
@@ -80,8 +80,6 @@ function displayMovements(movements) {
   });
 }
 
-displayMovements(account1.movements);
-
 //! COMPUTING USER NAMES
 /*
  *the user name should be first letter of each full name and should be lower case.
@@ -114,27 +112,68 @@ function calcBalance(movements) {
   const balance = movements.reduce((acc, el) => acc + el);
   labelBalance.textContent = `${balance}€`;
 }
-calcBalance(account1.movements);
 
 //! CALC ACCOUNT SUM
-const calcAccSum = function (movements) {
-  const incomes = movements
+const calcAccSum = function (acc) {
+  const incomes = acc.movements
     .filter((mov) => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumIn.textContent = `${incomes}€`;
 
-  const outcomes = movements
+  const outcomes = acc.movements
     .filter((mov) => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumOut.textContent = `${Math.abs(outcomes)}€`;
 
-  //* interest is 1.2% of each deposite and will only be paid if it's bigger than 1€
-  //* ex: if you deposite 80€, 1.2% * 80 = 0.96€ so it won't be paid
-  const interest = movements
+  //* interest is set in acc data of each deposite and will only be paid if it's bigger than 1€
+  //* ex: if you deposite 80€ and your rate is 1.2%, 1.2% * 80 = 0.96€ so it won't be paid
+  const interest = acc.movements
     .filter((mov) => mov > 0)
-    .map((deposit) => (deposit * 1.2) / 100)
+    .map((deposit) => (deposit * acc.interestRate) / 100)
     .filter((interest) => interest >= 1)
     .reduce((acc, interest) => acc + interest, 0);
   labelSumInterest.textContent = `${interest}€`;
 };
-calcAccSum(account1.movements);
+
+//! EMPLEMENT USER LOGIN LOGIC
+let currentAccount;
+
+btnLogin.addEventListener("click", (e) => {
+  // prevent form submit
+  e.preventDefault();
+
+  // set the account that username's matches as the current account
+  currentAccount = accounts.find(
+    (acc) => acc.username === inputLoginUsername.value
+  );
+
+  // will throw error if we enter an unknown username (ex: ek,cxz,...) cuz currentAccount will be undefined
+  // if (currentAccount.pin === Number(inputLoginPin.value)) {
+  //   console.log("login success");
+  // }
+
+  // &&
+  // if (currentAccount && currentAccount.pin === Number(inputLoginPin.value)) {
+  //   console.log("login success");
+  // }
+
+  // ?.
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    //* Display ui & welcome message
+    labelWelcome.textContent = `Welcome ${currentAccount.owner.split(" ")[0]}`;
+    containerApp.style.opacity = 1;
+
+    //* clear input fieldsvlaue and and lose focus after login
+    inputLoginUsername.value = inputLoginPin.value = "";
+    inputLoginPin.blur();
+
+    //* Display movements
+    displayMovements(currentAccount.movements);
+
+    //* Display balance
+    calcBalance(currentAccount.movements);
+
+    //* Display Summary
+    calcAccSum(currentAccount);
+  }
+});
